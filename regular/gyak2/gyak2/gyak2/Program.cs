7e7d92916c67d767fe300132432581d9e974d278
Jankovics Péter <jankovicsp21@ady-nagyatad.sulinet.hu>
2024-12-13 14:18:16 +0100

    sadasd

--- regular/gyak2/gyak2/gyak2/Program.cs
+++ regular/gyak2/gyak2/gyak2/Program.cs
@@ -10,4 +10,57 @@ Regex Minta = new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
 var talalat = Minta.Matches(adatok);
 //Console.WriteLine(talalat);
 
-Console.WriteLine(talalat.Count);
+//Console.WriteLine(talalat.Count);
+
+var kereses = "GET";
+
+//Regex Minta2 = new Regex(@"^(\a[g]\a[e]\a[t])");
+
+var pattern = Regex.Escape(kereses);
+
+var vegeredmeny = Regex.Matches(adatok, pattern);
+//Console.WriteLine(vegeredmeny.Count);
+
+
+
+Regex minta2 = new Regex(@"GET (/\s*)");
+
+var talalat2 = minta2.Matches(adatok);
+
+Console.WriteLine(talalat2.Count);
+
+
+
+//var talalat2 = Minta.Matches(adatok);
+//Console.WriteLine(talalat2.Count);
+
+Regex minta3 = new Regex(@"\S{3} (\d|\d{2}) \d{4}");
+
+var talalat3 = minta3.Matches(adatok);
+
+Console.WriteLine(talalat3.Count);
+/*
+foreach (Match elem in talalat3)
+{
+    Console.WriteLine(elem.Groups[1].Value);
+}
+*/
+Dictionary<string, int> kigyujt = new Dictionary<string, int>();
+
+
+foreach (Match elem in talalat3)
+{
+    if (kigyujt.ContainsKey(elem.Value))
+    {
+        kigyujt[elem.Value]++;
+    }
+    else
+    {
+        kigyujt.Add(elem.Value, 1);
+    }
+}
+
+foreach (KeyValuePair<string, int> elem in kigyujt)
+{
+    Console.WriteLine(elem);
+}
--- regular/gyak2/gyak2/gyak2/obj/Debug/net8.0/gyak2.AssemblyInfo.cs
+++ regular/gyak2/gyak2/gyak2/obj/Debug/net8.0/gyak2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gyak2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+035a69ac15e1bc6a04b9abfb88f95bda690b97e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac55ae0cd872c288edb54b0aa50b90c8b599177")]
 [assembly: System.Reflection.AssemblyProductAttribute("gyak2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gyak2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
