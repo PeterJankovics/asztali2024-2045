381b38b33c222e9a18af8be822d8ae849b2a8801
Jankovics Péter <jankovicsp21@ady-nagyatad.sulinet.hu>
2025-01-16 13:31:12 +0100

    asdasdasd

---
+++ rekurziv/gyak1/gyak1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+static BigInteger fact(int szam)
+{
+    BigInteger eredmeny = 1;
+    if (szam > 1)
+    {
+        eredmeny = fact(szam - 1);
+    }
+    else
+    {
+        return 1;
+    }
+    return szam * eredmeny;
+}
+
+
+Console.WriteLine(fact(10000));
